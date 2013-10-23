0d77d45232b260c94e844a4717f5344e6702dcee
Justin Chang <justin.chang@blackbaud.com>
2013-10-23 08:48:32 -0500

    first commit

---
+++ src/main/java/page/Page.java
@@ -0,0 +1,11 @@
+package main.java.page;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: justin
+ * Date: 10/23/13
+ * Time: 8:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Page {
+}
