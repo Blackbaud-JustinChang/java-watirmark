47146a36b062db67cfe934ad32b5bfef5d3e6002
Justin Chang <jchang691@gmail.com>
2013-10-26 00:43:25 -0500

    adds more features to setting elements in page test

--- src/test/java/PageView.java
+++ src/test/java/PageView.java
@@ -7,7 +7,8 @@ import org.openqa.selenium.By;
 public class PageView extends Page {
 
     public Keyword firstname = new Keyword(By.name("firstname"));
-    public Keyword select = new Keyword(By.id("company"));
+    public Keyword companySelect = new Keyword(By.id("company"));
+    public Keyword includeEmail = new Keyword(By.name("mailing_list"));
 
     public PageView(){
         createKeywords();
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -18,6 +18,12 @@ public class Test {
         File file = new File("src/test/resources/page_test.html");
         Page.browser().get(file.getAbsolutePath());
         view.firstname.set("Hello");
+        view.companySelect.set("Regional");
+        view.includeEmail.set("true");
+        view.includeEmail.set("True");
+        view.includeEmail.set("false");
+        view.includeEmail.set("False");
+        view.includeEmail.set("nonBoolean");
 
     }
 
