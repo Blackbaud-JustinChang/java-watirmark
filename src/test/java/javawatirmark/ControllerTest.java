f9941c0e7e4342195866307bb8fd7ec79d81be55
kyle.martinez <kmartinez@convio.com>
2013-10-30 10:59:09 -0500

    import missing libraries

--- src/test/java/javawatirmark/ControllerTest.java
+++ src/test/java/javawatirmark/ControllerTest.java
@@ -1,11 +1,14 @@
 package javawatirmark;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class ControllerTest {
@@ -26,24 +29,24 @@ public class ControllerTest {
 
     }
 
-//    @AfterClass
-//    public static void tearDown(){
-//
-//    }
-//
-//    @Test
-//    public void testControllerView(){
-//        assertEquals(controller.getView().getClass(), new PageView().getClass());
-//    }
-//
-//    @Test
-//    public void testControllerPopulation(){
-//        controller.create();
-//        assertEquals(view.firstname.get().getAttribute("value"), "Bobby");
-//        assertEquals(new Select(view.companySelect.get()).getFirstSelectedOption().getText(), "Regional");
-//        assert(view.includeEmail.get().isSelected());
-//        assert(view.gender.get("Male").isSelected());
-//    }
+    @AfterClass
+    public static void tearDown(){
+
+    }
+
+    @Test
+    public void testControllerView(){
+        assertEquals(controller.getView().getClass(), new PageView().getClass());
+    }
+
+    @Test
+    public void testControllerPopulation(){
+        controller.create();
+        assertEquals(view.firstname.get().getAttribute("value"), "Bobby");
+        assertEquals(new Select(view.companySelect.get()).getFirstSelectedOption().getText(), "Regional");
+        assert(view.includeEmail.get().isSelected());
+        assert(view.gender.get("Male").isSelected());
+    }
 
     @Test
     public void testControllerVerification() {
